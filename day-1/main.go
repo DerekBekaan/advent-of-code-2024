10c4bece17cba4d675943c4e622366af8e9bfb47
Derek Bekaan <derekbekaan@gmail.com>
2024-12-08 10:03:20 -0500

    Day 1 part 2

--- day-1/main.go
+++ day-1/main.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	input, _ := os.ReadFile("input.txt")
 
-	fmt.Printf("Part One: %d", partOne(string(input)))
+	fmt.Printf("Part One: %d\n", partOne(string(input)))
+	fmt.Printf("Part Two: %d", partTwo(string(input)))
 }
 
 func partOne(input string) int {
@@ -37,6 +38,32 @@ func partOne(input string) int {
 	sum := 0
 	for i := 0; i < len(l1); i++ {
 		sum += abs(l1[i] - l2[i])
+	}
+
+	return sum
+}
+
+func partTwo(input string) int {
+	inputs := strings.ReplaceAll(input, "\r", "")
+	lists := strings.Split(inputs, "\n")
+
+	l1 := make([]int, 0, len(lists))
+
+	counts := make(map[int]int)
+
+	for _, list := range lists {
+		ls := strings.Split(list, "   ")
+
+		i1, _ := strconv.Atoi(ls[0])
+		l1 = append(l1, i1)
+		
+		i2, _ := strconv.Atoi(ls[1])
+		counts[i2] += 1
+	}
+
+	sum := 0
+	for _, i := range l1 {
+		sum += i * counts[i]
 	}
 
 	return sum
--- day-1/main_test.go
+++ day-1/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestSumListSixDigits(t *testing.T) {
+func TestPartOne(t *testing.T) {
 	lists := `3   4
 4   3
 2   5
@@ -12,6 +12,22 @@ func TestSumListSixDigits(t *testing.T) {
 	expected := 11
 
 	actual := partOne(lists)
+
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	lists := `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+	expected := 31
+
+	actual := partTwo(lists)
 
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
