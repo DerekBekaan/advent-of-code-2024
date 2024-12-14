40c96121b6c49140863a8d4625fa82290069a7cc
Derek Bekaan <derekbekaan@gmail.com>
2024-12-14 11:11:22 -0500

    Day 3 remove comment

--- day-3/main_test.go
+++ day-3/main_test.go
@@ -16,7 +16,6 @@ func TestPartOne(t *testing.T) {
 
 func TestPartTwo(t *testing.T) {
 	input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
-	// input := `aabbccdd`
 
 	expected := 48
 	actual := partTwo(input)
