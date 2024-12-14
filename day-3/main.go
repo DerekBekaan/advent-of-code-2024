a5005d892ab6d3c845e08936d7772ac38c3af1d3
Derek Bekaan <derekbekaan@gmail.com>
2024-12-14 11:06:56 -0500

    Day 3 part 2

--- day-3/main.go
+++ day-3/main.go
@@ -12,6 +12,7 @@ func main() {
 	input, _ := os.ReadFile("input.txt")
 
 	fmt.Printf("Part One: %d\n", partOne(string(input)))
+	fmt.Printf("Part Two: %d\n", partTwo(string(input)))
 }
 
 func partOne(input string) int {
@@ -24,6 +25,30 @@ func partOne(input string) int {
 		y, _ := strconv.Atoi(mul[strings.Index(mul, ",") + 1: strings.Index(mul, ")")])
 
 		sum += x * y
+	}
+
+	return sum
+}
+
+func partTwo(input string) int {
+	re := regexp.MustCompile(`.*?mul\((?P<x>[0-9]+),(?P<y>[0-9]+)\)`)	
+	muls := re.FindAllStringSubmatch(input, -1)
+	
+	sum := 0
+	do := true
+	for _, m := range muls {
+		if strings.Contains(m[0], "don't") {
+			do = false
+			continue
+		}	else if strings.Contains(m[0], "do") {
+			do = true
+		}	
+
+		if do {
+			x, _ := strconv.Atoi(m[1])
+			y, _ := strconv.Atoi(m[2])
+			sum += x * y
+		}
 	}
 
 	return sum
--- day-3/main_test.go
+++ day-3/main_test.go
@@ -2,12 +2,24 @@ package main
 
 import "testing"
 
+
 func TestPartOne(t *testing.T) {
 	input := `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-	// input := `mul(2,4)`
 
 	expected := 161
 	actual := partOne(input)
+
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
+	// input := `aabbccdd`
+
+	expected := 48
+	actual := partTwo(input)
 
 	if actual != expected {
 		t.Errorf("Expected %v, got %v", expected, actual)
